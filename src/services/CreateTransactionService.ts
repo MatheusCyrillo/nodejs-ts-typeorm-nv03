d89f03d1aa69412d85ea5dc2b3f1aec6ba158779
MatheusCyrillo <marc.cyrillo@gmail.com>
2020-09-02 23:42:50 -0300

    incluindo typeorm nas models, rota de criacao de transacao feita

--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,10 +1,47 @@
 // import AppError from '../errors/AppError';
 
 import Transaction from '../models/Transaction';
+import { getRepository } from 'typeorm';
+import Category from '../models/Category';
+
+interface Request {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+  category_title: string;
+}
 
 class CreateTransactionService {
-  public async execute(): Promise<Transaction> {
-    // TODO
+
+  public async execute({
+    title,
+    category_title,
+    type,
+    value,
+  }: Request): Promise<Transaction> {
+    const categoryRepository = getRepository(Category);
+
+    let category = await categoryRepository.findOne({
+      title: category_title,
+    });
+
+    if (!category) {
+      category = categoryRepository.create({
+        title: category_title,
+      });
+
+      await categoryRepository.save(category);
+    }
+
+    const transactionRepository = getRepository(Transaction);
+    const transaction = transactionRepository.create({
+      title,
+      type,
+      value,
+      category_id: category.id,
+    });
+
+    return await transactionRepository.save(transaction);
   }
 }
 
