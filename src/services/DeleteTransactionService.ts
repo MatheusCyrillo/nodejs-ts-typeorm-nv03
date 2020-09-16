391d5f4bfd779b17df3dbe7d8dbfa744fea28dba
MatheusCyrillo <marc.cyrillo@gmail.com>
2020-09-16 01:46:50 -0300

    rota de import quase pronta

--- src/services/DeleteTransactionService.ts
+++ src/services/DeleteTransactionService.ts
@@ -1,21 +1,25 @@
-import AppError from '../errors/AppError';
 import { EntityNotFoundError } from 'typeorm/error/EntityNotFoundError';
-import TransactionsRepository from '../repositories/TransactionsRepository';
 import { getCustomRepository } from 'typeorm';
+
+import AppError from '../errors/AppError';
+
+import TransactionsRepository from '../repositories/TransactionsRepository';
+
 
 class DeleteTransactionService {
   public async execute(id: string): Promise<void> {
-    try{
-    const transactionsRepo = getCustomRepository(TransactionsRepository);
+    try {
+      const transactionsRepo = getCustomRepository(TransactionsRepository);
 
-    const transaction = await transactionsRepo.findOneOrFail(id);
+      const transaction = await transactionsRepo.findOneOrFail(id);
 
-    await transactionsRepo.remove(transaction);
-    }
-    catch(e){
-      if(e instanceof EntityNotFoundError){
-        throw new AppError(`The Transaction ${id} could not be deleted because it does not exist`);
-      }
+      await transactionsRepo.remove(transaction);
+    } catch (e) {
+      if (e instanceof EntityNotFoundError) {
+        throw new AppError(
+          `The Transaction ${id} could not be deleted because it does not exist`,
+        );
+      } else throw new AppError(e.message, 500);
     }
   }
 }
--- src/services/ImportTransactionsService.ts
+++ src/services/ImportTransactionsService.ts
@@ -1,8 +1,29 @@
+import csvParser from 'csv-parser';
+import fs from 'fs';
+
 import Transaction from '../models/Transaction';
 
+import TransactionsRepository from '../repositories/TransactionsRepository';
+import { getCustomRepository } from 'typeorm';
+import { resolveConfig } from 'prettier';
+
 class ImportTransactionsService {
-  async execute(): Promise<Transaction[]> {
-    // TODO
+  async execute(filePath: string): Promise<Transaction[]> {
+    let transactions: Transaction[] = [];
+
+    const transactionRepo = getCustomRepository(TransactionsRepository);
+
+    return new Promise((resolve) => {
+      fs.createReadStream(filePath)
+        .pipe(csvParser())
+        .on('data', async (transaction: Transaction) =>
+          transactions.push(transaction),
+        )
+        .on('end', async () => {
+          transactions = transactionRepo.create(transactions);
+          resolve(await transactionRepo.save(transactions));
+        });
+    });
   }
 }
 
